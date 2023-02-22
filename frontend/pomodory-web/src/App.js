1928fc5368d0edb05b7c08c2bd407c67cdc15415
KyleTownsend74 <townsend.kyle.kt@gmail.com>
2023-02-21 20:25:07 -0800

    Add connection to backend
    Co-Authored-By: Nhi Nguyen <conniewongntn@gmail.com>
    Co-Authored-By: kendrickofmars <57419700+kendrickofmars@users.noreply.github.com>

--- frontend/pomodory-web/src/App.js
+++ frontend/pomodory-web/src/App.js
@@ -5,14 +5,19 @@ function App() {
     return (
         <div className="App">
             <h1>Guppy Buddies Test Page!</h1>
-        
-        
-        <input id="id_inp" type="text"></input>
-        <button class="btn btn-primary" onclick="search_by_id()">Search</button>  
-        <script src = "main.py"></script>
+            <input id="id_inp" type="text"></input>
+            <button className="btn btn-primary" onClick={() => search()}>Search</button>  
+            <script src = "main.py"></script>
         </div>
     );
 }
+
+async function search() {
+    await fetch('http://localhost:5000/collection/001')
+        .then((response) => response.json())
+        .then((data) => console.log(data));
+}
+
 //we now want to create the search functionality using the prof's example in class
 // export function search(){
 //     return(
