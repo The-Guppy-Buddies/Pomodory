a166a97ad3c356e39db9de1f604a40148e51afc3
kendrickofmars <ahhadmukhtar@gmail.com>
2023-04-22 19:02:09 -0700

    added day.js in package-lock.json and package.json, using countdownRef for starting and stopping timer

--- frontend/pomodory-web/src/components/CountdownTimer.js
+++ frontend/pomodory-web/src/components/CountdownTimer.js
@@ -1,12 +1,12 @@
 import './CountdownTimer.css';
-import Countdown from 'react-countdown';
-import { useState } from 'react';
+import Countdown, { CountdownApi } from 'react-countdown';
+import { useState, useRef } from 'react';
 import sound from '../assets/owaowa.mp3'
 
 function Timer() {
 
-    const [time, setTime] = useState(Date.now() + 10000);
-
+    const [time, setTime] = useState(1000);
+    const countdownRef = useRef();
     function play() {
         new Audio(sound).play()
     }
@@ -15,9 +15,14 @@ function Timer() {
        <div className='timer'>
             <p>
                 Countdown timer:
-                <Countdown date={time} onComplete={play}></Countdown>
+                <Countdown ref ={countdownRef} date={time} onComplete={play}></Countdown>
                 
             </p>
+            {/* <input name="Add time" type="number"></input> */}
+            {/* <button className="btn btn-primary" onClick={() => setTime (time+1000)}>Add 1m</button> */}
+            <button className="btn btn-primary" onClick={() => countdownRef.current.api.start()}>start</button>
+            <button className="btn btn-primary" onClick={() => countdownRef.current.api.pause()}>Pause</button>
+            <button className="btn btn-primary" onClick={() => setTime(time + 1000)}>You have to name it</button>
        </div> 
     );
 }
