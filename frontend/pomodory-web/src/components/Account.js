a84e361cba79c0240db138daad0b216733aea654
KyleTownsend74 <55927311+KyleTownsend74@users.noreply.github.com>
2023-04-26 18:49:03 -0700

    Require input when creating guppy

--- frontend/pomodory-web/src/components/Account.js
+++ frontend/pomodory-web/src/components/Account.js
@@ -57,9 +57,9 @@ function Account({ url, setUserFunc }) {
             </div>
             <div id="create-user" className="hidden">
                 <form action={`${url}/create`} method="post">
-                    <input className="mt-2 form-control" name="User ID" type="text" placeholder='User ID'></input>
-                    <input className="mt-2 form-control" name="username" type="text" placeholder='Username'></input>
-                    <input className="mt-2 form-control" name="Guppy No." type="text" placeholder='(1-5)'></input>
+                    <input className="mt-2 form-control" name="User ID" type="text" placeholder='User ID' required></input>
+                    <input className="mt-2 form-control" name="username" type="text" placeholder='Username' required></input>
+                    <input className="mt-2 form-control" name="Guppy No." type="text" placeholder='(1-5)' required></input>
                     <button className="btn btn-primary" type="submit">Submit</button>
                     <button className="btn btn-secondary" type="button" onClick={() => backToAccountScreen()}>Back</button>
                 </form>
